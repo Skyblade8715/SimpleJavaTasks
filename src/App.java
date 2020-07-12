9a78c44c98d7a002f63e536c277be79877b60357
Skyblade <piotr8715@gmail.com>
2020-07-12 18:49:25 +0200

    Task 3 - FibonacciSeries.java - count fibonacci series up to given value. Task 2 - Added second method of counting sum of digits in number as well as considered situation in which given number is negative.

--- src/App.java
+++ src/App.java
@@ -8,9 +8,10 @@ public class App {
         System.out.println(sentence);
         System.out.println(ReverseWordsInSentence.reverse(sentence));
         SumOnlyNumWithGivenSum summator = new SumOnlyNumWithGivenSum(9);
-        List<Integer> testingIntegerList = new ArrayList<>(Arrays.asList(27, 31, 32, 9, 23, 111111, 63)); //For 9 as desiredSum expected result is 27+9+63 which is 99.
+        List<Integer> testingIntegerList = new ArrayList<>(Arrays.asList(27, 31, 32, 9, 23, 111111, 63, -9)); //For 9 as desiredSum expected result is 27+9+63-9 which is 90.
         System.out.println(summator.countSum(testingIntegerList));
 
+        System.out.println(FibonacciSeries.countFibonacciIteratively(133));
     }
 }
 
--- src/SumOnlyNumWithGivenSum.java
+++ src/SumOnlyNumWithGivenSum.java
@@ -8,10 +8,11 @@ public class SumOnlyNumWithGivenSum {
     }
 
     //Task - count sum of integers in a list but only using numbers which sum of digits equals a desiredSum.
+    //Disclaimer: Numbers like -27 are considered to have sum of 9, while during addition they are consider as -27
     public int countSum(List<Integer> numList){
         int result = 0;
         for(Integer i : numList){
-            if(countSumOfNumber(i) == desiredSum){
+            if(Math.abs(countSumOfNumber(i)) == desiredSum){
                 result += i;
             }
         }
@@ -20,9 +21,32 @@ public class SumOnlyNumWithGivenSum {
 
     private int countSumOfNumber(int num){
         int result = 0;
-        while (num > 0){
-            result += num % 10;
-            num /= 10;
+        if(num >= 0) {
+            while (num > 0) {
+                result += num % 10;
+                num /= 10;
+            }
+        } else {
+            while (num < 0) {
+                result += num % 10;
+                num /= 10;
+            }
+        }
+        return result;
+    }
+
+    //Second method for counting sum of digits in a number - just for funsies
+    private int countSumOfNumberV2(int num){
+        int result = 0;
+        String[] StringValue = String.valueOf(num).split("");
+        if(num >= 0){
+            for(String s : StringValue){
+                result += Integer.parseInt(s);
+            }
+        } else {
+            for(int i = 1; i < StringValue.length; i++){
+                result += Integer.parseInt(StringValue[i]);
+            }
         }
         return result;
     }
